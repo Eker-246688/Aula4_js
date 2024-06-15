fbceee8a03456a2cb099852108026e4a8ec633de
MARCOS TADEU DA CUNHA <166663288+Eker-246688@users.noreply.github.com>
2024-06-15 15:29:49 -0300

    Add files via upload

---
+++ boaTarde.js
@@ -0,0 +1,18 @@
+function tarde() {
+ 
+let hora = prompt('Digite a hora por favor?');
+let bomDia = 'Bom dia!!';
+let boaTarde = 'Já posso desejar boa tarde!!';
+
+if(hora >= 0, hora <= 12){
+  alert( hora = bomDia);
+  
+}
+
+
+if(hora >= 12, hora <= 18){
+  alert( hora = boaTarde);
+  
+}
+
+    }
---
+++ votoEFacultativo..js
@@ -0,0 +1,18 @@
+function voto() {
+       
+  let idade = prompt('digite sua idade');
+  let votoEFacultativo = 'voto é facultativo';
+  let obrigatorioVoto = 'Voto Obrigatório';
+
+  if(idade <= 18, idade >= 70){
+    alert( idade = votoEFacultativo);
+    
+  }
+  
+
+  if(idade >= 18, idade <= 70){
+    alert( idade = obrigatorioVoto);
+    
+  }
+ 
+      }
